6e52fcaa77d8e5a137f180e2c570094470bec6d1
Mehtab Anjum <mehtab.anjum@nagarro.com>
2018-05-15 22:18:29 +0530

    change server.js

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,10 @@ import cbRoute from "./route/course";
 import subjectRoute from "./route/subject";
 import teacherRoute from "./route/teacher";
 
+var cors = require('cors');
+
 const app = express();
+app.use(cors);
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
